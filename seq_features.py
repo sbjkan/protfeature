04ac9d2ce89373db1d433ef2a0c69b7afc464d19
sbjkan <sbjkan@caltech.edu>
2017-06-23 11:28:41 -0700

    Initial commit of protfeature

---
+++ seq_features.py
@@ -0,0 +1,11 @@
+def n_neg(seq):
+    """Number of negative residues a protein sequence"""
+
+    # Convert sequence to upper case
+    seq = seq.upper()
+
+    if seq == 'Z':
+        raise RuntimeError('Z is not a valid amino acid.')
+
+    # Count E's and D's, since these are the negative residues
+    return seq.count('E') + seq.count('D')
